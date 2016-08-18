dba1c3e4dbc80acd8ede7dc8a4622f8be678ea8d
Atahualpa S. Kraemer <ata.kraemer@gmail.com>
2016-08-18 18:50:36 -0500

    Update README.mk

--- Tareas/README.mk
+++ Tareas/README.mk
@@ -1,1 +1,6 @@
+La tarea 3 aún no se entrega. Se completará hasta la siguiente clase. Pero pueden ir descargando el notebook, con la parte que se vió en clase. 
+
 En los notebooks en esta página no se ven a veces bien las ecuaciones (depende de tu sistema operativo y tu explorador). Por eso es recomendable que descargen los notebooks y los corran en su cuenta de juliabox, o en su computadora si ya tienen instalado Julia, Iphyton e IJulia. 
+
+Se recuerda que las tareas son personales. Si se detecta fraude, esto podría llevarles a un NA. Sólo cuentan un 30%, así que no
+vale la pena correr el riesgo de ser descubiertos en un fraude. 
