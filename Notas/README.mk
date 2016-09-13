7f59cf138c8df5faa77765b0e646f7df62d39328
Atahualpa S. Kraemer <ata.kraemer@gmail.com>
2016-09-13 18:26:46 -0500

    Update README.mk

--- Notas/README.mk
+++ Notas/README.mk
@@ -2,3 +2,6 @@ Aquí encontrarás notas y links interesantes sobre el cúrso.
 
 La documentación sobre Julia la puedes encontrar en: http://julialang.org/
 
+Agregué el archivo "Metodos2.jl" Podrán usarlo para hacer la práctica. 
+
+**Espero que se diviertan con la práctica** 
