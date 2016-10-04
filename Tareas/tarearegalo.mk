aaf7f734a9e2ad39c2d791a5a2426da2aef8017e
Atahualpa S. Kraemer <ata.kraemer@gmail.com>
2016-10-04 15:42:16 -0700

    Create tarearegalo.mk

---
+++ Tareas/tarearegalo.mk
@@ -0,0 +1,6 @@
+# Tarea de regalo
+
+En esta tarea lo que tienen que hacer es que coincidan colores y cabezas con colas en un cuadrado de 3x3 para los ajolotes de la imagen.
+
+El primero en resolverla tendrá 1 punto sobre la calificación final!!! 
+
